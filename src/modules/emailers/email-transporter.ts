e76bbb2810c08d11afc118146c83607529cb8782
CodeNoJutsu <bellokhalid74@gmail.com>
2024-08-29 11:43:55 +0100

    Modified mailing

--- src/modules/emailers/email-transporter.ts
+++ src/modules/emailers/email-transporter.ts
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = (sender: string, password: string) => {
     const transporter = nodemailer.createTransport({
-        host: 'mail.botsub.com.ng', // Replace with your DNS provider's SMTP host
-        port: 465, // Replace with appropriate port (e.g., 465 for SSL/TLS)
+        host: process.env.EMAIL_HOST, // Replace with your DNS provider's SMTP host
+        port: process.env.EMAIL_PORT, // Replace with appropriate port (e.g., 465 for SSL/TLS)
         secure: true, // Use true for SSL/TLS encryption (recommended)
         auth: {
             user: sender, // Replace with your email address

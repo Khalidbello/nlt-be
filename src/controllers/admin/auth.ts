5b3f4afaa96b796ee37e8e3442c125de14e84eb5
CodeNoJutsu <bellokhalid74@gmail.com>
2024-09-30 20:52:34 +0100

    Added log for admin logins

--- src/controllers/admin/auth.ts
+++ src/controllers/admin/auth.ts
@@ -6,15 +6,20 @@ const adminLoginHandler = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     try {
-        const response = await queryAdminByEmail(email);
-        if (response.length > 0 && response[0].password === password) {
+        const response = await queryAdminByEmail(email.trim());
+        if (response.length > 0 && response[0].password === password.trim()) {
+            console.log('Admin found', email, password);
+
             (req.session as CustomSessionData).user = {
                 email: email,
                 type: 'admin',
                 id: response[0].id
             }
             return res.status(200).json({ message: 'logged in succesfully' });
-        };
+
+        } else {
+            console.log('Admin not found email not co', email, password);
+        }
         res.status(404).json({ message: 'admin with cridentials not found' });
     } catch (err) {
         console.error('error signing in admin', err);
--- src/services/admin/auth.ts
+++ src/services/admin/auth.ts
@@ -2,12 +2,13 @@ import pool from "../../modules/connnect-db";
 
 // query ti fetch admin by id
 const queryAdminByEmail = (email: string) => {
+    console.log('Email in admin query', email);
     return new Promise<any>((resolve, reject) => {
         const query = 'SELECT * FROM admins WHERE email = ?';
 
-        pool.query(query, [email], (err, result) => {
+        pool.query(query, [email.trim()], (err, result) => {
             if (err) return reject(err);
-
+            console.log('admin login query result', result);
             resolve(result);
         })
     });

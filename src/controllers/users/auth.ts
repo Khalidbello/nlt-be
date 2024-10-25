720d36e270b466eeb3a66b9945c242b4c61bb1ff
CodeNoJutsu <bellokhalid74@gmail.com>
2024-10-25 19:53:38 +0100

    Added log for request before cors for debugging

--- src/controllers/users/auth.ts
+++ src/controllers/users/auth.ts
@@ -1,111 +1,134 @@
-import { Request, Response } from 'express';
-import { checkUserExist, createNewUser } from '../../services/users/user-queries';
-import { CustomSessionData } from '../../types/session-types';
-import { checkUserExistType } from '../../types/general';
-import emailOtpSender from '../../modules/emailers/email-otp';
-import otpGenerator from '../../modules/otp-generator';
-import { queryDeleteOtp, queryOtp } from '../../services/otp-queries';
-
+import { Request, Response } from "express";
+import {
+  checkUserExist,
+  createNewUser,
+} from "../../services/users/user-queries";
+import { CustomSessionData } from "../../types/session-types";
+import { checkUserExistType } from "../../types/general";
+import emailOtpSender from "../../modules/emailers/email-otp";
+import otpGenerator from "../../modules/otp-generator";
+import { queryDeleteOtp, queryOtp } from "../../services/otp-queries";
 
 // function to handle user login
 const logInHandler = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  console.log("In the login handler....... see header ", req.header);
+  const { email, password } = req.body;
 
-    try {
-        const response: checkUserExistType[] = await checkUserExist(email);
+  try {
+    const response: checkUserExistType[] = await checkUserExist(email);
 
-        if (response.length > 0 && response[0].password === password) {
-            (req.session as CustomSessionData).user = {
-                email: email,
-                type: 'normal',
-                id: response[0].user_id
-            }
-            return res.status(200).json({ message: 'logged in succesfully' });
-        };
-        res.status(404).json({ message: 'user with cridentials not found' });
-    } catch (err) {
-        console.error('error in login handler', err);
-        res.status(500).json({ message: err });
-    };
+    if (response.length > 0 && response[0].password === password) {
+      (req.session as CustomSessionData).user = {
+        email: email,
+        type: "normal",
+        id: response[0].user_id,
+      };
+      return res.status(200).json({ message: "logged in succesfully" });
+    }
+    res.status(404).json({ message: "user with cridentials not found" });
+  } catch (err) {
+    console.error("error in login handler", err);
+    res.status(500).json({ message: err });
+  }
 };
 
-
-
-// function to handle creating of new accont 
+// function to handle creating of new accont
 const createAccountHandler = async (req: Request, res: Response) => {
-    const { firstName, lastName, email, password, phoneNumber, gender } = req.body;
-    const date = new Date();
+  console.log("hosttttttt............................", req.header);
 
-    try {
-        const response = await checkUserExist(email);
+  const { firstName, lastName, email, password, phoneNumber, gender } =
+    req.body;
+  const date = new Date();
 
-        if (response.length > 0) {
-            return res.status(409).json({ message: 'user exist' });
-        };
+  try {
+    const response = await checkUserExist(email);
 
-        const created = await createNewUser(firstName, lastName, email, password, phoneNumber, gender, date);
+    if (response.length > 0) {
+      return res.status(409).json({ message: "user exist" });
+    }
 
-        if (created.affectedRows > 0) {
-            (req.session as CustomSessionData).user = {
-                email: email,
-                type: 'normal',
-                // @ts-ignore
-                id: created.insertId
-            };
-            return res.json({ message: 'account created successfully' });
-        };
-        throw 'unable to create new user';
-    } catch (err) {
-        console.error('error in create accont handler', err);
-        res.status(500).json({ message: 'An error occured trying to create acount' });
-    };
+    const created = await createNewUser(
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      gender,
+      date
+    );
+
+    if (created.affectedRows > 0) {
+      (req.session as CustomSessionData).user = {
+        email: email,
+        type: "normal",
+        // @ts-ignore
+        id: created.insertId,
+      };
+      return res.json({ message: "account created successfully" });
+    }
+    throw "unable to create new user";
+  } catch (err) {
+    console.error("error in create accont handler", err);
+    res
+      .status(500)
+      .json({ message: "An error occured trying to create acount" });
+  }
 };
 
-
-// function to checkif user exist 
+// function to checkif user exist
 const passwordRecoveryCheckUser = async (req: Request, res: Response) => {
-    try {
-        const email: string = req.body.email;
-        const exist = await checkUserExist(email);
+  try {
+    const email: string = req.body.email;
+    const exist = await checkUserExist(email);
 
+    if (!exist[0])
+      return res
+        .status(404)
+        .json({ message: "user with credentials not found." });
 
-        if (!exist[0]) return res.status(404).json({ message: 'user with credentials not found.' });
+    // ts-ignore
+    const opt: number = await otpGenerator(exist[0].user_id);
+    // send otp email
+    emailOtpSender(email, exist[0].first_name, opt);
 
-        // ts-ignore
-        const opt: number = await otpGenerator(exist[0].user_id);
-        // send otp email
-        emailOtpSender(email, exist[0].first_name, opt);
-
-        res.json({ message: 'user exist' });
-    } catch (err) {
-        console.error('error in passwordRecoveryCheckUser', err);
-        res.status(500).json({ message: 'An error occured trying to find account' });
-    };
+    res.json({ message: "user exist" });
+  } catch (err) {
+    console.error("error in passwordRecoveryCheckUser", err);
+    res
+      .status(500)
+      .json({ message: "An error occured trying to find account" });
+  }
 };
-
 
 // function to confirm otp if valid retrun user password
 const passwordRecoveryConfirmOtp = async (req: Request, res: Response) => {
-    try {
-        const otp = req.body.otp;
-        const email = req.body.email;
+  try {
+    const otp = req.body.otp;
+    const email = req.body.email;
 
-        if (!email || !otp) return res.status(400).json({ message: 'incomlete data sent to server' });
+    if (!email || !otp)
+      return res.status(400).json({ message: "incomlete data sent to server" });
 
-        const user = await checkUserExist(email);
-        const dbOtp = await queryOtp(user[0].user_id);
-        const equal: boolean = otp === dbOtp?.otp;
+    const user = await checkUserExist(email);
+    const dbOtp = await queryOtp(user[0].user_id);
+    const equal: boolean = otp === dbOtp?.otp;
 
-        if (!equal) return res.status(401).json({ status: equal });
+    if (!equal) return res.status(401).json({ status: equal });
 
-        await queryDeleteOtp(user[0].user_id);
+    await queryDeleteOtp(user[0].user_id);
 
-        res.json({ password: user[0].password });
-    } catch (err) {
-        console.error('error in passwordRecoveryCheckUser', err);
-        res.status(500).json({ message: 'An error occured tryng get user password' });
-    };
+    res.json({ password: user[0].password });
+  } catch (err) {
+    console.error("error in passwordRecoveryCheckUser", err);
+    res
+      .status(500)
+      .json({ message: "An error occured tryng get user password" });
+  }
 };
 
-
-export { logInHandler, createAccountHandler, passwordRecoveryCheckUser, passwordRecoveryConfirmOtp };
+export {
+  logInHandler,
+  createAccountHandler,
+  passwordRecoveryCheckUser,
+  passwordRecoveryConfirmOtp,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,94 @@
 //ngrok http --domain=weekly-settled-falcon.ngrok-free.app 5000
 
-
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-
-import express, { NextFunction, Request, Response } from 'express';
-import session from 'express-session';
-import cors from 'cors';
-import { initiateDbConnection } from './modules/connnect-db';
-import auth from './routes/auth';
-import admin from './routes/admin';
-import users from './routes/users';
-import paymentGateWay from './routes/payment-gateway';
-
+import express, { NextFunction, Request, Response } from "express";
+import session from "express-session";
+import cors from "cors";
+import { initiateDbConnection } from "./modules/connnect-db";
+import auth from "./routes/auth";
+import admin from "./routes/admin";
+import users from "./routes/users";
+import paymentGateWay from "./routes/payment-gateway";
 
 // Create an Express application
 const app = express();
 const port = 5000;
 
-
 // environment configurations
-if (process.env.NODE_ENV === 'development') {
-    process.env.FLW_PB_KEY = process.env.FLW_TEST_PB_KEY;
-    process.env.FLW_SCRT_KEY = process.env.FLW_TEST_SCRT_KEY;
-    process.env.FLW_H = process.env.FLW_H_TEST;
+if (process.env.NODE_ENV === "development") {
+  process.env.FLW_PB_KEY = process.env.FLW_TEST_PB_KEY;
+  process.env.FLW_SCRT_KEY = process.env.FLW_TEST_SCRT_KEY;
+  process.env.FLW_H = process.env.FLW_H_TEST;
 }
 
 // environment configurations
-if (process.env.NODE_ENV === 'production') {
-    process.env.FLW_PB_KEY = process.env.FLW_PB_KEY;
-    process.env.FLW_SCRT_KEY = process.env.FLW_SCRT_KEY;
-    process.env.FLW_H = process.env.FLW_H;
-    process.env.DB_NAME = 'nlt-prod';
+if (process.env.NODE_ENV === "production") {
+  process.env.FLW_PB_KEY = process.env.FLW_PB_KEY;
+  process.env.FLW_SCRT_KEY = process.env.FLW_SCRT_KEY;
+  process.env.FLW_H = process.env.FLW_H;
+  process.env.DB_NAME = "nlt-prod";
 }
 
 // lockingin middle wears
-// cors config
-// cors config
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log("Received request:", {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+  });
+  next();
+});
+
+// cors confi
 const corsOption = {
-    origin: [' http://localhost', 'https://lifestyleleverage.com.ng', 'http://lifestyleleverage.com.ng'], // Replace with your frontend's origin
-    credentials: true
+  origin: [
+    " http://localhost",
+    "https://lifestyleleverage.com.ng",
+    "http://lifestyleleverage.com.ng",
+  ], // Replace with your frontend's origin
+  credentials: true,
 };
 
 const sessionOption = {
-    secret: 'your-secret-key', // Replace with a strong secret
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false, // Set to true if using HTTPS
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 // 1 day
-    }
+  secret: "your-secret-key", // Replace with a strong secret
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false, // Set to true if using HTTPS
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+  },
 };
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '10mb' }));
 app.use(cors(corsOption));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
 // @ts-ignore
-app.use(session(sessionOption))
-
+app.use(session(sessionOption));
 
 // adding routes as middle wears
-app.use('/auth', auth);
-app.use('/users', users);
-app.use('/admin', admin);
-app.use('/gateway', paymentGateWay);
-
+app.use("/auth", auth);
+app.use("/users", users);
+app.use("/admin", admin);
+app.use("/gateway", paymentGateWay);
 
 // Define a route handler for the root path
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
-
 
 // Catch-all route to handle 404 errors
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).send(`404 Not Found in :::::::::: path ${ req.url} `);
+  res.status(404).send(`404 Not Found in :::::::::: path ${req.url} `);
 });
-
 
 // Error-handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // app begins to listen after db connection
